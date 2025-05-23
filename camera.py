35774a39f6ffe4b9781d7288d96bb7892358cd62
Jeremy Janluigi Doria <jjdoriaupv@gmail.com>
2025-05-23 12:06:42 +0200

    camera working correctly

--- camera.py
+++ camera.py
@@ -1,17 +1,16 @@
-import cv2
+import subprocess
 from datetime import datetime
 import os
 
 def take_photo():
-    cam = cv2.VideoCapture(0)
-    ret, frame = cam.read()
-    cam.release()
-    if not ret:
-        return None
-
     if not os.path.exists("static"):
         os.makedirs("static")
 
     filename = f"static/photo_{datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg"
-    cv2.imwrite(filename, frame)
-    return filename
+    
+    try:
+        subprocess.run(["libcamera-jpeg", "-o", filename], check=True)
+        return filename
+    except subprocess.CalledProcessError:
+        return None
+
