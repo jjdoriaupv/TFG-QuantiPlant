c7627d4627618ff854423db6695e45afc342a7ec
Jeremy Janluigi Doria <jjdoriaupv@gmail.com>
2025-05-23 12:40:23 +0200

    camera .py updated to send images to server

--- camera.py
+++ camera.py
@@ -1,16 +1,28 @@
 import subprocess
 from datetime import datetime
 import os
+import requests
 
 def take_photo():
     if not os.path.exists("static"):
         os.makedirs("static")
 
-    filename = f"static/photo_{datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg"
-    
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    local_path = f"static/photo_{timestamp}.jpg"
+
     try:
-        subprocess.run(["libcamera-jpeg", "-o", filename], check=True)
-        return filename
+        subprocess.run(["libcamera-jpeg", "-o", local_path], check=True)
+
+        with open(local_path, 'rb') as f:
+            files = {'image': (f"photo_{timestamp}.jpg", f, 'image/jpeg')}
+            response = requests.post("http://<IP_PC>:6000/upload", files=files)
+            if response.status_code == 200:
+                print(f"[UPLOAD] Foto enviada correctamente")
+            else:
+                print(f"[UPLOAD] Error al subir: {response.text}")
+
+        return local_path
     except subprocess.CalledProcessError:
         return None
+
 
