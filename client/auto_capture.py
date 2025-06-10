f9d084e9d0a9a7d0842b56f33c5a87ee74e0b6c9
Jeremy Janluigi Doria <jjdoriaupv@gmail.com>
2025-06-10 09:15:17 +0200

    auto-caqpture

--- client/auto_capture.py
+++ client/auto_capture.py
@@ -1,10 +1,11 @@
 import threading
 import time
 from camera import take_photo
-from config_state import config
+from config_state import get_config
 
 def loop():
     while True:
+        config = get_config()
         if config['enabled']:
             print("[AUTO] Tomando foto automática...")
             take_photo()
@@ -15,4 +16,3 @@ def loop():
 def start_auto_capture():
     t = threading.Thread(target=loop, daemon=True)
     t.start()
-
