cdb04e490b8afcef166ec11aff50494bc149447d
Jeremy Janluigi Doria <jjdoriaupv@gmail.com>
2025-06-10 09:13:32 +0200

    config_state persistent

--- client/config_state.py
+++ client/config_state.py
@@ -1,25 +1,46 @@
-config = {
+import json
+import os
+
+CONFIG_FILE = 'config.json'
+
+default_config = {
     'enabled': False,
     'interval': 10,
     'exposure': 1000,
     'led_auto': False
 }
 
+def load_config():
+    if not os.path.exists(CONFIG_FILE):
+        save_config(default_config)
+    with open(CONFIG_FILE, 'r') as f:
+        return json.load(f)
+
+def save_config(data):
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(data, f)
+
 def get_config():
-    return config
+    return load_config()
 
 def enable_capture(enabled):
+    config = load_config()
     config['enabled'] = enabled
+    save_config(config)
 
 def set_interval(interval):
+    config = load_config()
     config['interval'] = interval
+    save_config(config)
 
 def set_exposure(exposure):
     if exposure > 60000:
         exposure = 60000
+    config = load_config()
     config['exposure'] = exposure
+    save_config(config)
 
 def set_led_auto(value):
+    config = load_config()
     config['led_auto'] = bool(value)
-
-
+    save_config(config)
