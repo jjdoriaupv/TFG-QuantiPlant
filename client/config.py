646ef688d50306a6f334b8d4caf4c93db3b40d0e
jjdoria <72863049+jjdoria@users.noreply.github.com>
2025-06-25 10:09:28 +0200

    cambio en las ip

--- client/config.py
+++ client/config.py
@@ -1,5 +1,4 @@
 import os
 
-SERVER_URL = os.environ.get("SERVER_URL", "http://192.168.1.53:5001")
+SERVER_URL = os.environ.get("SERVER_URL", "http://192.168.232.127:5001")
 RPI_ID = os.environ.get("DEVICE_ID", "rpi-1")
-
