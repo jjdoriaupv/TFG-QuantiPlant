4237c6493c052702960c83af529c1dfa054760a2
Jeremy Janluigi Doria <jjdoriaupv@gmail.com>
2025-08-16 12:30:37 +0200

    autocaptura normal corregida

--- core/auto_capture.py
+++ core/auto_capture.py
@@ -3,23 +3,34 @@ import time
 from core.camera import take_photo
 from core.config_state import get_config, save_config
 
+_state_lock = threading.Lock()
+_worker = None
+_worker_lock = threading.Lock()
+
 def loop():
-    last_burst_time = 0
+    last_burst_time = 0.0
     burst_intervals_done = 0
     session_active = False
+    next_due = None
 
     while True:
-        config = get_config()
-        enabled = config.get('enabled', False)
+        with _state_lock:
+            config = get_config()
+            enabled = config.get('enabled', False)
 
         if enabled and not session_active:
             session_active = True
-            config['photos_taken'] = 0
-            last_burst_time = 0
+            last_burst_time = 0.0
             burst_intervals_done = 0
-            save_config(config)
+            next_due = time.monotonic()
+            with _state_lock:
+                config = get_config()
+                config['photos_taken'] = 0
+                save_config(config)
 
         if enabled:
+            with _state_lock:
+                config = get_config()
             burst_mode_enabled = config.get('burst_mode_enabled', False)
 
             if burst_mode_enabled:
@@ -28,17 +39,21 @@ def loop():
                 interval_between_bursts = config.get('interval_between_bursts', 3600)
                 burst_interval = config.get('burst_interval', 60)
 
-                now = time.time()
-                if burst_intervals_done < max_intervals and now - last_burst_time >= interval_between_bursts:
+                now = time.monotonic()
+                if burst_intervals_done < max_intervals and (last_burst_time == 0.0 or now - last_burst_time >= interval_between_bursts):
                     for i in range(burst_count):
                         take_photo()
                         if i < burst_count - 1:
                             time.sleep(burst_interval)
                     burst_intervals_done += 1
-                    last_burst_time = time.time()
+                    last_burst_time = time.monotonic()
                 elif burst_intervals_done >= max_intervals:
-                    config['enabled'] = False
-                    save_config(config)
+                    with _state_lock:
+                        config = get_config()
+                        config['enabled'] = False
+                        save_config(config)
+                else:
+                    time.sleep(0.5)
             else:
                 max_photos = config.get('max_photos', 0)
                 interval_min = config.get('interval', 10)
@@ -48,39 +63,59 @@ def loop():
                 burst_mode = not led_enabled and not led_auto
 
                 if max_photos == 0:
-                    take_photo()
-                    time.sleep(interval_sec)
-
+                    now = time.monotonic()
+                    if next_due is None or now >= next_due:
+                        take_photo()
+                        next_due = now + interval_sec
+                    else:
+                        time.sleep(min(1.0, max(0.0, next_due - now)))
                 elif burst_mode:
-                    now = time.time()
-                    if now - last_burst_time >= interval_sec:
+                    now = time.monotonic()
+                    if next_due is None or now - last_burst_time >= interval_sec:
                         exposure = config.get('exposure', 1000000)
                         delay = (exposure / 1_000_000.0) + 5.0
-                        for i in range(max_photos):
+                        for _ in range(max_photos):
                             take_photo()
                             time.sleep(delay)
-                        last_burst_time = time.time()
-                        config['enabled'] = False
-                        save_config(config)
+                        last_burst_time = time.monotonic()
+                        with _state_lock:
+                            config = get_config()
+                            config['enabled'] = False
+                            save_config(config)
                     else:
-                        time.sleep(1)
+                        time.sleep(0.5)
                 else:
-                    photos_taken = config.get('photos_taken', 0)
-                    if photos_taken < max_photos:
+                    now = time.monotonic()
+                    if next_due is None:
+                        next_due = now
+                    if now >= next_due:
                         take_photo()
-                        config['photos_taken'] = photos_taken + 1
-                        save_config(config)
-                        time.sleep(interval_sec)
+                        with _state_lock:
+                            config = get_config()
+                            photos_taken = config.get('photos_taken', 0) + 1
+                            config['photos_taken'] = photos_taken
+                            save_config(config)
+                        if photos_taken >= max_photos:
+                            with _state_lock:
+                                config = get_config()
+                                config['enabled'] = False
+                                save_config(config)
+                        else:
+                            next_due = now + interval_sec
                     else:
-                        config['enabled'] = False
-                        save_config(config)
+                        time.sleep(min(1.0, max(0.0, next_due - now)))
         else:
             if session_active:
                 session_active = False
-                last_burst_time = 0
+                last_burst_time = 0.0
                 burst_intervals_done = 0
+                next_due = None
             time.sleep(1)
 
 def start_auto_capture():
-    t = threading.Thread(target=loop, daemon=True)
-    t.start()
+    global _worker
+    with _worker_lock:
+        if _worker is not None and _worker.is_alive():
+            return
+        _worker = threading.Thread(target=loop, daemon=True)
+        _worker.start()
